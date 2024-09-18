9cb5c9a1e3a0a704d6eb550df5fd8bf34327fd90
jasir <muhammedjasirch@gmail.com>
2024-09-18 22:06:24 +0530

    updates

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,21 +1,17 @@
-import  Apps  from 'pm2';
-
-const config = [
-  {
-    name: 'my-app',
-    script: 'src/index.ts',
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    interpreter: 'ts-node',
-    interpreter_args: '--project tsconfig.json',
-    env: {
-      NODE_ENV: 'development',
-    },
-    env_production: {
-      NODE_ENV: 'production',
-    },
-  },
-];
-
-module.exports = { apps: config };
+module.exports = {
+  apps: [
+    {
+      name: 'life-drop',           
+      script: 'src/index.ts',       
+      interpreter: './node_modules/.bin/ts-node', 
+      watch: true,               
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,               
+      },
+    }
+  ]
+}
