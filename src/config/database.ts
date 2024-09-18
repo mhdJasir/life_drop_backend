e6e2d6ca029f1b8b5adb562fc44afd21081bb9a3
jasir <muhammedjasirch@gmail.com>
2024-09-18 22:57:00 +0530

    changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
 import fs from 'fs';
 import path from 'path';
-require("dotenv").config();
+import dotenv from "dotenv"
+dotenv.config();
 
 const configPath = path.join(__dirname, 'config.json');
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+console.log(process.env.PASSWORD);
 
 const environment = process.env.NODE_ENV || 'development'; 
 const dbConfig = config[environment];
