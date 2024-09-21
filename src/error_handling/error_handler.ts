6e1268b99fff5c0243b15399c499c868aa6d0678
jasir <muhammedjasirch@gmail.com>
2024-09-21 22:52:41 +0530

    c

--- src/error_handling/error_handler.ts
+++ src/error_handling/error_handler.ts
@@ -13,7 +13,8 @@ const errorHandler = (err: any, req: Request, res: Response, next: NextFunction)
     
     return res.status(500).json({
         message: 'Server error',
-        errors: "Something went wrong"
+        errors: err,
+
     });
 };
 
